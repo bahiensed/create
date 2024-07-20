3d73cddf41cce524deee1003ed9b9e28b0c80d49
dbahiense <douglas@rohling.com.br>
2024-07-20 13:50:58 -0300

    create left sidebar links

---
+++ constants/index.ts
@@ -0,0 +1,12 @@
+export const LeftSideBarLinks = [
+  {
+    route: '/home',
+    label: 'Home',
+    imgURL: '/icons/home.svg',
+  },
+  {
+    route: '/profile',
+    label: 'Profile',
+    imgURL: '/icons/microphone.svg',
+  },
+]
