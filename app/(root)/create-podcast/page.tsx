dbdd024e44c5429b62a31d56d7fc72b3e549b7da
dbahiense <douglas@rohling.com.br>
2024-07-22 15:08:24 -0300

    create other app pages

--- app/(root)/create-podcast/page.tsx
+++ app/(root)/create-podcast/page.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const CreatePodcast = () => {
   return (
-    <div>CreatePodcast</div>
+    <div>
+      <section className='flex flex-col gap-5'>
+        <h1 className='font-bold text-20 text-white-1'>Create Podcast</h1>
+      </section>
+    </div>
   )
 }
 
--- app/(root)/discover/page.tsx
+++ app/(root)/discover/page.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Discover = () => {
   return (
-    <div>Discover</div>
+    <div>
+      <section className='flex flex-col gap-5'>
+        <h1 className='font-bold text-20 text-white-1'>Discover</h1>
+      </section>
+    </div>
   )
 }
 
