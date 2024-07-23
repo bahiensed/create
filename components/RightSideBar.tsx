08ea75f81c7d7d9b7c45cb3f08bff8947c0225fb
dbahiense <douglas@rohling.com.br>
2024-07-23 10:09:19 -0300

    insert clerk button

--- components/RightSideBar.tsx
+++ components/RightSideBar.tsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
 
 const RightSideBar = () => {
   return (
     <section className='right_sidebar text-white-1 '>
+      <div>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </div>
+
       Right SideBar
     </section>
   )
