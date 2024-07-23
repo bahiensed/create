b7c04b3a6ed4fa1b8f4db8769b05de79304c7cfd
dbahiense <douglas@rohling.com.br>
2024-07-23 10:10:29 -0300

    setup convex

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://evident-skink-35.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ]
+};
+
+export default authConfig;
